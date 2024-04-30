c16738f13bf0bc6fd26dc82e7a91097eb2ff07b8
Sparsh Garg <sparsh@nec-labs.com>
2024-04-30 15:56:35 -0700

    fixed basic cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,6 +27,8 @@ def go(args):
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
     df = df.dropna()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv('clean_sample.csv', index=False)
     artifact = wandb.Artifact(
         name=args.output_artifact,
